4c0774e51b57d4c7b47b21e07cdba2091459a2cf
Jubayer Shikder <validjubayer@gmail.com>
2025-05-20 17:37:31 +0600

    Add MongoDB String

--- index.js
+++ index.js
@@ -11,6 +11,49 @@ app.use(cors());
 
 
 // mongodb
+const uri = `mongodb+srv://${process.env.db_user}:${process.env.db_pass}}@cluster0.eriw8fe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+
+const client = new MongoClient(uri, {
+
+  serverApi: {
+
+    version: ServerApiVersion.v1,
+
+    strict: true,
+
+    deprecationErrors: true,
+
+  }
+
+});
+
+async function run() {
+
+  try {
+
+    // Connect the client to the server	(optional starting in v4.7)
+
+    await client.connect();
+
+    // Send a ping to confirm a successful connection
+
+    await client.db("admin").command({ ping: 1 });
+
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+  } finally {
+
+    // Ensures that the client will close when you finish/error
+
+    // await client.close();
+
+  }
+
+}
+
+run().catch(console.dir);
 
 // port listening
 app.listen(port);
