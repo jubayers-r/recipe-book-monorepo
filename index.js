09a4d6850390294a298b8bf774c4976781dc83e1
Jubayer Shikder <validjubayer@gmail.com>
2025-05-22 22:08:32 +0600

    Add MyRecipe Delete Api

--- index.js
+++ index.js
@@ -43,7 +43,13 @@ async function run() {
       const result = await RecipeDB.find(query).toArray();
       res.send(result);
     });
- 
+
+    app.delete("/myRecipes/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await RecipeDB.deleteOne(query);
+      res.send(result);
+    });
 
     app.post("/recipes", async (req, res) => {
       const recipeDetails = req.body;
