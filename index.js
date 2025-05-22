01f99e22c06bf8da36e645c2f0cf1fc89bd61529
Jubayer Shikder <validjubayer@gmail.com>
2025-05-22 22:07:34 +0600

    Add My Recipe Query Api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -10,47 +10,52 @@ const port = process.env.PORT || 3000;
 app.use(express.json());
 app.use(cors());
 
-
 // mongodb
 const uri = `mongodb+srv://${process.env.db_user}:${process.env.db_pass}@cluster0.eriw8fe.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 
 const client = new MongoClient(uri, {
-
   serverApi: {
-
     version: ServerApiVersion.v1,
 
     strict: true,
 
     deprecationErrors: true,
-
-  }
-
+  },
 });
 
 async function run() {
-
   try {
-
     // Connect the client to the server	(optional starting in v4.7)
 
     const UsersDB = client.db("recipe_app").collection("users");
     const RecipeDB = client.db("recipe_app").collection("recipes");
 
     // recipe related apis
-    app.post('/recipes', async(req, res) => {
+    app.get("/recipes", async (req, res) => {
+      const result = await RecipeDB.find().toArray();
+      res.send(result);
+    });
+    app.get("/myRecipes", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const result = await RecipeDB.find(query).toArray();
+      res.send(result);
+    });
+ 
+
+    app.post("/recipes", async (req, res) => {
       const recipeDetails = req.body;
       const result = await RecipeDB.insertOne(recipeDetails);
       res.send(result);
-    })
+    });
     // user related apis
-    app.post('/users', async(req, res) => {
+    app.post("/users", async (req, res) => {
       const userProfile = req.body;
       const result = await UsersDB.insertOne(userProfile);
       res.send(result);
-    })
+    });
 
     await client.connect();
 
@@ -58,16 +63,13 @@ async function run() {
 
     await client.db("admin").command({ ping: 1 });
 
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
-
     // Ensures that the client will close when you finish/error
-
     // await client.close();
-
   }
-
 }
 
 run().catch(console.dir);
