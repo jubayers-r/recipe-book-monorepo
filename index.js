4133cdd34e02afdc3015695b34d26aa4cb2166f7
Jubayer Shikder <validjubayer@gmail.com>
2025-05-23 11:04:41 +0600

    Deploy on Vercel

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
+require("dotenv").config();
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -99,15 +99,15 @@ async function run() {
       res.send(result);
     });
 
-    await client.connect();
+    // await client.connect();
 
     // Send a ping to confirm a successful connection
 
-    await client.db("admin").command({ ping: 1 });
+    // await client.db("admin").command({ ping: 1 });
 
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
