80f9cde28e44e057bd71c21f84f41aeacbc70b50
Jubayer Shikder <validjubayer@gmail.com>
2025-05-23 22:22:23 +0600

    Add 'TypeWriter' and 'Awesome Reveal' in HomePage

--- src/components/TopRecipes.jsx
+++ src/components/TopRecipes.jsx
@@ -1,23 +1,40 @@
 import React from "react";
 import RecipeCard from "./RecipeCard";
 import { Link } from "react-router";
+import { useTypewriter } from "react-simple-typewriter";
+import { Slide, Fade } from "react-awesome-reveal";
 
 const TopRecipes = ({ topRecipes }) => {
+  const [text] = useTypewriter({
+    words: [
+      "Top Recipes",
+      "Most Loved Plates",
+      "Crave-Worthy Dishes",
+      "Tasty Treasures",
+      "Best Picks",
+    ],
+    loop: 0,
+  });
+
   return (
     <div className="w-9/11 mx-auto">
-      <h1 className="text-4xl font-bold text-center my-5">Top Recipes</h1>
-      <div className="grid lg:grid-cols-2 xl:grid-cols-3 gap-5">
-        {topRecipes.map((recipe) => (
-          <RecipeCard recipe={recipe} />
-        ))}
-      </div>
+      <h1 className="sm:text-4xl text-2xl font-bold text-center my-5 min-h-[50px]">
+        {text}
+      </h1>
+      <Slide direction="left">
+        <Fade duration={2000}>
+          <div className="grid lg:grid-cols-2 xl:grid-cols-3 gap-5">
+            {topRecipes.map((recipe) => (
+              <RecipeCard recipe={recipe} />
+            ))}
+          </div>
+        </Fade>
+      </Slide>
       <div className="flex justify-center">
         <Link to={"/recipes"}>
-        <button
-          className="btn hover:bg-[#00ed64] hover:rounded-full hover:border hover:border-black  mt-5 btn-lg flex w-fit"
-        >
-          See All Recipes
-        </button>
+          <button className="btn hover:bg-[#00ed64] hover:rounded-full hover:border hover:border-black  mt-5 btn-lg flex w-fit">
+            See All Recipes
+          </button>
         </Link>
       </div>
     </div>
